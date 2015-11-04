956ea2e305780c1c7b6aa0c65d1f8e35db6b5588
aishan <aishan520315@163.com>
2015-11-04 13:03:58 +0800

    Update bearychat.php

--- bearychat.php
+++ bearychat.php
@@ -8,6 +8,7 @@
  * 由于bearychat的outgoing机器人是post指定类型数据到某处，而图灵机器人接收信息是get方式，并且图灵机器人返回的数据形式之于Bearychat机器人
  * 需要接收的返回数据类型也是有一定的出入，所以需要此文件做个中转
  */
+header("Content-type: application/json");
 require_once 'curl.php';//引入curl类
 $content = file_get_contents("php://input");//读取bearychat机器人post过来的数据
 $content_arr=json_decode($content);//json转对象
